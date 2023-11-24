c63dff8db60d2d86a063ff908632405eb40722ec
Amos Fang <48528755+amosfang@users.noreply.github.com>
2023-11-24 14:53:03 +0100

    update config file

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "4c7662b6-35dd-4329-af86-2172c58ea894")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
